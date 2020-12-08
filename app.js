2bf20c784ddfe901c14a21f933a0f24a33133d94
pato7b <56084546+pato7b@users.noreply.github.com>
2020-12-08 22:07:52 +0100

    updade
    Update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const blogRoutes = require('./routes/blogRoutes');
-const PORT = process.env.PORT || 6000
+const PORT = process.env.PORT || 5000
 
 // express app
 const app = express();
